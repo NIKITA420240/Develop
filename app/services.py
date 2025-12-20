690a8a647862f6465d37af0c64ac488686f8e4c8
Nikita <nikitasuperme.028384@gmail.com>
2025-12-20 13:58:17 +0300

    Complete Weather API task

--- app/services.py
+++ app/services.py
@@ -1,9 +1,11 @@
 import httpx
 import os
 from fastapi import HTTPException
+from dotenv import load_dotenv
 
-# Ключ лучше хранить в .env файле
-API_KEY = os.getenv("OPENWEATHER_API_KEY") 
+load_dotenv()
+
+API_KEY = "69f16b8c1709a69d8a2166066035b82a"
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 class WeatherService:
@@ -23,13 +25,19 @@ class WeatherService:
             raise HTTPException(status_code=400, detail="City or Coordinates required")
 
         async with httpx.AsyncClient() as client:
+            print(f"🔑 МОЙ КЛЮЧ: {API_KEY}")
             try:
                 response = await client.get(BASE_URL, params=params)
                 response.raise_for_status() # Вызовет ошибку, если статус не 200
                 data = response.json()
                 return data["main"]["temp"]
             except httpx.HTTPStatusError as e:
-                # Если город не найден (404) или ошибка ключа (401)
-                raise HTTPException(status_code=e.response.status_code, detail="External API Error")
+                print(f"🔴 ОШИБКА ОТ OPENWEATHER: {e.response.status_code}")
+                print(f"🔴 ТЕКСТ ОШИБКИ: {e.response.text}")
+                # Выводим реальный текст ошибки прямо в браузер
+                raise HTTPException(status_code=e.response.status_code, detail=e.response.text)
+            # except httpx.HTTPStatusError as e:
+            #     # Если город не найден (404) или ошибка ключа (401)
+            #     raise HTTPException(status_code=e.response.status_code, detail="External API Error")
             except Exception as e:
                 raise HTTPException(status_code=503, detail="Weather service unavailable")
